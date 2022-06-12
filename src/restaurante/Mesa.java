71cfa03954ac7918adecdbd085ac6e8a1b1df453
João Pedro <77790402+jpclrocha@users.noreply.github.com>
2022-06-12 17:06:09 -0300

    nao ta imprimindo

--- src/restaurante/Mesa.java
+++ src/restaurante/Mesa.java
@@ -1,16 +1,18 @@
 package restaurante;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
-public class Mesa {
+public class Mesa implements Serializable {
+    ArrayList<Produto> pedidos = new ArrayList<>();
     private String nome;
     private String entrada;
     private String saida;
 
-    public Mesa(String nome){
-        this.nome = nome;
+    public Mesa(){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         this.entrada = dtf.format(now);
@@ -41,4 +43,30 @@ public class Mesa {
         LocalDateTime now = LocalDateTime.now();
         this.saida = dtf.format(now);
     }
+
+    public void adicionaPedido(int codigo, ArrayList<Produto> estoque){
+        for(Produto p : estoque){
+            if(p.getCodido() == codigo){
+                pedidos.add(p);
+            }else{
+                System.out.println("Nao existe um produto com esse codigo");
+            }
+        }
+    }
+
+    public double getConta(){
+        double total = 0;
+        for(Produto p : pedidos){
+            total += p.getCodido();
+        }
+        return total;
+    }
+
+    public String toString(){
+        String palavra = "";
+        palavra += "Nome do cliente: "+getNome();
+        palavra += "Horario de entrada: "+getEntrada();
+        return palavra;
+    }
+
 }
